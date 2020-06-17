f61318ec9c94fe884096001f608aa75de934c4d4
dnfatec <danilo@fatecitapetininga.edu.br>
2020-06-17 19:51:45 -0300

    Teste git2

--- app/src/main/java/com/example/extestes/MainActivity.java
+++ app/src/main/java/com/example/extestes/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "oi", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "oi ", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "alterado git", Toast.LENGTH_LONG).show();
         retiraFoto();
 
